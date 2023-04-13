d4f0b3281206c5aa80b625dc06014e1228fbf0e9
xiangcitoZ <pzxiang1@gmail.com>
2023-04-13 13:22:29 +0200

    Add project files.

---
+++ ApiEmpleadosMultiplesRutas/Data/EmpleadosContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiEmpleadosMultiplesRutas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEmpleadosMultiplesRutas.Data
+{
+    public class EmpleadosContext: DbContext
+    {
+        public EmpleadosContext
+           (DbContextOptions<EmpleadosContext> options) :
+               base(options)
+        { }
+
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
